71c8c19e3699761a07c0061fab4485f1d271c47e
Yaroslav <petuxov-06@bk.ru>
2026-05-13 14:23:21 +0300

    Modify css styles. Add animations

--- site/app.js
+++ site/app.js
@@ -1,4 +1,4 @@
-const API_BASE = localStorage.getItem("apiBase") || "http://127.0.0.1:8000";
+const API_BASE = localStorage.getItem("apiBase") || "http://localhost:8000";
 
 const themeSelect = document.getElementById("themeSelect");
 const loginForm = document.getElementById("loginForm");
